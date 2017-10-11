8930d0dae5a887f494d047c48c2b59c42e0f1f3f
kazuki-nisshin <nisshin@dtdsh.com>
2017-10-11 16:31:20 +0900

    サテライトサイトのフッターと共通化

--- theme/footer.php
+++ theme/footer.php
@@ -1,5 +1,14 @@
+<?php get_template_part( './elements/cta--footer' ); ?>
 <footer class="footer" role="contentinfo">
-	<?php get_template_part( './elements/cta--footer' ); ?>
+<div class="footer--social">
+		<a href="<?php echo home_url(); ?>" class="bg-teal button--circle"><?php NID_SVG::icon( 'home', array( 'class' => 'footer--social__icon' ), 'トップページへ' ); ?></a>
+		<a href="https://www.facebook.com/yamashitakolawoffice" class="button--circle bg-facebook" target="_blank"><?php NID_SVG::icon( 'facebook', array( 'class' => 'footer--social__icon' ), 'Facebook' ); ?></a>
+		<a href="https://plus.google.com/118124010942091667362?hl=ja" class="button--circle bg-google-plus" target="_blank"><?php NID_SVG::icon( 'google-plus', array( 'class' => 'footer--social__icon' ), 'Google+' ); ?></a>
+		<a href="https://www.youtube.com/channel/UCQepvNppunUj6BSQgAtbx1g" class="button--circle bg-youtube" target="_blank"><?php NID_SVG::icon( 'youtube', array( 'class' => 'footer--social__icon' ), 'Youtube' ); ?></a>
+		<a href="tel:0120783409" title="電話する" class="button--circle bg-facebook show-for-medium"><?php NID_SVG::icon( 'phone', array( 'class' => 'footer--social__icon' ), '電話する' ); ?></a>
+		<a href="<?php echo get_permalink( get_page_by_path( 'contact' ) ); ?>" title="お問い合わせ" class="button--circle bg-pink show-for-medium"><?php NID_SVG::icon( 'mail', array( 'class' => 'footer--social__icon' ), 'お問い合わせ' ); ?></a>
+		<a href="#top" class="button--circle bg-lime hide-for-medium"><?php NID_SVG::icon( 'arrow-up', array( 'class' => 'footer--social__icon' ), 'ページ上部へ' ); ?></a>
+	</div>
 	<div class="row">
 		<div class="column small-12 large-8">
 			<p class="footer--title"><?php NID_SVG::icon( 'site-map', array( 'class' => 'footer--title-icon' ) ); ?>サイトマップ</p>
@@ -29,16 +38,7 @@
 			<p><a href="<?php echo get_permalink( get_page_by_path( 'contact' ) ); ?>">info@law-yamashita.com</a></p>
 		</div>
 	</div>
-	<div class="footer--social">
-		<a href="<?php echo home_url(); ?>" class="bg-teal button--circle"><?php NID_SVG::icon( 'home', array( 'class' => 'footer--social__icon' ), 'トップページへ' ); ?></a>
-		<a href="https://www.facebook.com/yamashitakolawoffice" class="button--circle bg-facebook" target="_blank"><?php NID_SVG::icon( 'facebook', array( 'class' => 'footer--social__icon' ), 'Facebook' ); ?></a>
-		<a href="https://plus.google.com/118124010942091667362?hl=ja" class="button--circle bg-googleplus" target="_blank"><?php NID_SVG::icon( 'google-plus', array( 'class' => 'footer--social__icon' ), 'Google+' ); ?></a>
-		<a href="https://www.youtube.com/channel/UCQepvNppunUj6BSQgAtbx1g" class="button--circle bg-youtube" target="_blank"><?php NID_SVG::icon( 'youtube', array( 'class' => 'footer--social__icon' ), 'Youtube' ); ?></a>
-		<a href="tel:0120783409" title="電話する" class="button--circle bg-facebook show-for-medium"><?php NID_SVG::icon( 'phone', array( 'class' => 'footer--social__icon' ), '電話する' ); ?></a>
-		<a href="<?php echo get_permalink( get_page_by_path( 'contact' ) ); ?>" title="お問い合わせ" class="button--circle bg-pink show-for-medium"><?php NID_SVG::icon( 'mail', array( 'class' => 'footer--social__icon' ), 'お問い合わせ' ); ?></a>
-		<a href="#top" class="button--circle bg-lime hide-for-medium"><?php NID_SVG::icon( 'arrow-up', array( 'class' => 'footer--social__icon' ), 'ページ上部へ' ); ?></a>
-		<p class="footer--copy">© <span itemprop="copyrightYear"><?php echo date( 'Y' ); ?></span> <?php bloginfo( 'name' ); ?></p>
-	</div>
+	<p class="footer--copy">© <span itemprop="copyrightYear"><?php echo date( 'Y' ); ?></span> <?php bloginfo( 'name' ); ?></p>
 	<div id="js--button-fixed" class="button--fixed"><a href="#top" title="<?php bloginfo( 'name' ); ?>" class="button--circle bg-lime"><?php NID_SVG::icon( 'arrow-up', array( 'class' => 'footer--social__icon' ), 'ページ上部へ' ); ?></a></div>
 </footer>
 <?php get_template_part( './elements/footer-menu' ); ?>
