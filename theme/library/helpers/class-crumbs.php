9d370e60bae96d37d7537230bd06cc38b6f12ab3
kazuki-nisshin <nisshin@dtdsh.com>
2018-04-02 19:14:15 +0900

    パンくずリスト

--- theme/library/helpers/class-crumbs.php
+++ theme/library/helpers/class-crumbs.php
@@ -1,45 +1,16 @@
 <?php
-/**
- * WordPress Breadcrumb Utilities
- * to support BEM class namings
- * conventions
- *
- * @author Max G J Panas <http://maxpanas.com>
- */
 
-
-/**
- * Class NID_Crumbs
- *
- */
 class NID_Crumbs {
 
 
-	/**
-	 * Print the breadcrumbs HTML
-	 * based on the given menu
-	 * location
-	 *
-	 * @param string $theme_location
-	 * @param array  $options
-	 */
-	public static function crumbs( $theme_location = 'primary', $options = array() ) {
-		echo self::get_crumbs( $theme_location, $options );
+	public static function crumbs( $options = array() ) {
+		echo self::get_crumbs( $options );
 	}
 
 
-	/**
-	 * Return the breadcrumbs HTML
-	 * based on the given menu
-	 * location
-	 *
-	 * @param string $theme_location
-	 * @param array  $options
-	 *
-	 * @return string
-	 */
-	public static function get_crumbs( $theme_location = 'primary', $options = array() ) {
-		$breadcrumbs_items = self::get_crumbs_array( $theme_location, $options );
+	
+	public static function get_crumbs( $options = array() ) {
+		$breadcrumbs_items = self::get_crumbs_array( $options );
 		if ( ! $breadcrumbs_items || empty( $breadcrumbs_items ) ) {
 			return '';
 		}
@@ -79,73 +50,225 @@ class NID_Crumbs {
 	}
 
 
-	/**
-	 * Get the breadcrumbs based on
-	 * a menu location for the
-	 * current content
-	 *
-	 * @param string $theme_location
-	 * @param array  $options
-	 *
-	 * @return array
-	 */
-	public static function get_crumbs_array( $theme_location = 'primary', $options = array() ) {
+
+	public static function get_crumbs_array( $options = array() ) {
 		$clean_options = wp_parse_args( $options, array(
 			'home_title'   => 'トップ'
 		) );
 
 		$current_item = self::get_current_crumb_item();
-		$crumbs       = array( $current_item );
+		$crumbs = array( $current_item );
 
-		if ( $current_item['id'] && ( $parents = NID_Menu::get_parent_menu_items( $theme_location, $current_item['id'] ) ) ) {
-			$crumbs = array_merge( self::menu_items_to_crumbs( $parents ), $crumbs );
+		if ( 'single' === $current_item['type'] || 'page' === $current_item['type'] || 'taxonomy' === $current_item['type'] ) {
+			$parents = self::get_parent_crumb_items( $current_item );
+			if ( $parents ) $crumbs = array_merge( $parents, $crumbs );
 		}
 
 		if ( ! is_front_page() ) {
-			array_unshift( $crumbs, self::get_crumb_item( 'home', home_url(), $clean_options['home_title'], array(
+			array_unshift( $crumbs, self::job_crumb_item( 'home', home_url(), $clean_options['home_title'], array(
 				'type' => 'home'
 			) ) );
 		}
 
-		return apply_filters( 'NID_crumbs_array', $crumbs, $theme_location, $options );
+		return apply_filters( 'NID_crumbs_array', $crumbs, $options );
 	}
 
 
-	/**
-	 * Transform menu items into
-	 * breadcrumb items
-	 *
-	 * @param $menu_items
-	 *
-	 * @return array
-	 */
-	public static function menu_items_to_crumbs( $menu_items ) {
+
+	public static function get_parent_crumb_items( $current_item ) {
+		global $post;
 		$crumbs = array();
-		foreach ( $menu_items as $i => $menu_item ) {
-			$crumbs[] = self::get_crumb_item(
-				$menu_item->object_id,
-				$menu_item->url,
-				$menu_item->title
-			);
-		}
 
-		return $crumbs;
+		switch ( $current_item['type'] ) :
+			case 'single':
+				$post_type = get_post_type( $post->ID );
+				// POST
+				if ( 'post' === $post_type ) {
+					
+					// 本体サイト用トピックスリンク
+					$crumbs[] = self::job_crumb_item(
+						'544',
+						get_page_link( '544' ),
+						get_the_title( '544' )
+					);
+					
+					// カテゴリーを表示
+					$categories = get_the_category( $post->ID );
+					$cat = self::get_youngest_cat( $categories );
+					if ( 0 !== $cat->parent ) {
+						$ancestors = array_reverse( get_ancestors( $cat->cat_id, 'category' ) );
+						foreach ( $ancestors as $ancestor ) {
+							$crumbs[] = self::job_crumb_item(
+								$ancestor,
+								get_category_link( $ancestor ),
+								get_cat_name( $ancestor )
+							);
+						}
+					}
+					$crumbs[] = self::job_crumb_item(
+						$cat->cat_id,
+						get_category_link( $cat->cat_id ),
+						get_cat_name( $cat->cat_id )
+					);
+
+				// カスタム投稿タイプ
+				} else {
+					// カスタム投稿アーカイブ
+					$crumbs[] = self::job_crumb_item(
+						$post_type,
+						get_post_type_archive_link( $post_type ),
+						get_post_type_object( $post_type )->label
+					);
+
+					$taxonomy = get_object_taxonomies( $post_type )[0];
+					$taxes = get_the_terms( $post->ID, $taxonomy );
+					$tax = self::get_youngest_tax( $taxes, $taxonomy );
+					if ( 0 !== $tax->parent ) {
+						$ancestors = array_reverse( get_ancestors( $tax->term_id, $taxonomy ) );
+						foreach ( $ancestors as $ancestor ) {
+							$crumbs[] = self::job_crumb_item(
+								$ancestor,
+								get_term_link( $ancestor, $taxonomy ),
+								get_term( $ancestor, $taxonomy )
+							);
+						}
+					}
+					$crumbs[] = self::job_crumb_item(
+						$tax->term_id,
+						get_term_link( $tax, $taxonomy ),
+						$tax->name
+					);
+				}
+				return $crumbs;
+				break;
+
+			case 'taxonomy':
+				$is_taxonomy = get_queried_object( 'taxonomy' );
+				$cpt = get_query_var( 'taxonomy' );
+				if ( 'category' !== $is_taxonomy->taxonomy ) {
+					$taxonomy = get_queried_object();
+					$post_types = get_taxonomy( $cpt )->object_type;
+					$post_type = $post_types[0];
+
+					// カスタム投稿アーカイブ
+					$crumbs[] = self::job_crumb_item(
+						$post_type,
+						get_post_type_archive_link( $post_type ),
+						get_post_type_object( $post_type )->label
+					);
+
+					// 親があれば表示
+					if ( 0 !== $taxonomy->parent ) {
+						$ancestors = array_reverce( get_ancestors( $taxonomy->term_id, $taxonomy->taxonomy ) );
+						foreach ( $ancestors as $ancestor ) {
+							$crumbs[] = self::job_crumb_item(
+								$ancestor,
+								get_term_link( $ancestor, $taxonomy ),
+								get_term( $ancestor, $taxonomy )
+							);
+						}
+					}
+					return $crumbs;
+				} else {
+					// 本体サイト用トピックスリンク
+					$crumbs[] = self::job_crumb_item(
+						'544',
+						get_page_link( '544' ),
+						get_the_title( '544' )
+					);
+					
+					// カテゴリーを表示
+					$cat = get_queried_object();
+					if ( 0 !== $cat->parent ) {
+						$ancestors = array_reverse( get_ancestors( $cat->cat_id, 'category' ) );
+						foreach ( $ancestors as $ancestor ) {
+							$crumbs[] = self::job_crumb_item(
+								$ancestor,
+								get_category_link( $ancestor ),
+								get_cat_name( $ancestor )
+							);
+						}
+					}
+					return $crumbs;
+				}
+				break;
+			
+			case 'page':
+				if ( 0 !== $post->post_parent ) {
+					$ancestors = array_reverse( get_post_ancestors( $post->ID ) );
+					foreach ( $ancestors as $ancestor ) {
+						$crumbs[] = self::job_crumb_item(
+							$ancestor,
+							get_permalink( $ancestor ),
+							get_the_title( $ancestor )
+						);
+					}
+				}
+				return $crumbs;
+				break;
+
+			endswitch;
 	}
 
 
-	/**
-	 * Get the breadcrumb item
-	 * for the content being
-	 * currently viewed
-	 *
-	 * @return array
-	 */
+	public static function get_youngest_cat( $categories ) {
+		if ( 1 === count( $categories ) ) {
+			$youngest = $categories[0];
+		} else {
+			$count = 0;
+			foreach ( $categories as $category ) {
+				$children = get_term_children( $category->term_id, 'category' );
+				if ( $children ) {
+					if ( $count < count( $children ) ) {
+						$count = $count( $children );
+						$lot_children = $children;
+						foreach ( $lot_children as $child ) {
+							if ( in_category( $child, $post->ID ) ) {
+								$youngest = get_category( $child );
+							}
+						}
+					}
+				} else {
+					$youngest = $category;
+				}
+			}
+		}
+		return $youngest;
+	}
+
+
+	public static function get_youngest_tax( $taxes, $taxonomy ) {
+		if ( 1 === count( $taxes ) ) {
+			$youngest = $taxes[key($taxes)];
+		} else {
+			$count = 0;
+			foreach ( $taxes as $tax ) {
+				$children = get_term_children( $tax->term_id, $taxonomy );
+				if ( $children ) {
+					if ( $count < count( $children ) ) {
+						$count = $count( $children );
+						$lot_children = $children;
+						foreach ( $lot_children as $child ) {
+							if ( is_object_in_term( $post->ID, $taxonomy ) ) {
+								$youngest = get_term( $child, $taxonomy );
+							}
+						}
+					}
+				} else {
+					$youngest = $tax;
+				}
+			}
+		}
+		return $youngest;
+	}
+
+
 	public static function get_current_crumb_item() {
 		$item_id = $type = $url = $title = false;
 
 		if ( is_search() ) {
 			$url     = get_search_link();
-			$title   = __( 'Search' );
+			$title   = '「' . wp_specialchars( $s, 1 ) . '」で検索した結果';
 			$type    = 'search';
 
 		} else if ( is_author() ) {
@@ -184,7 +307,7 @@ class NID_Crumbs {
 			$type    = 'page';
 		}
 
-		return self::get_crumb_item( $item_id, $url, $title, array(
+		return self::job_crumb_item( $item_id, $url, $title, array(
 			'current' => true,
 			'type'    => $type
 		) );
@@ -202,7 +325,7 @@ class NID_Crumbs {
 	 *
 	 * @return array
 	 */
-	protected static function get_crumb_item( $item_id, $link, $text, $flags = array() ) {
+	protected static function job_crumb_item( $item_id, $link, $text, $flags = array() ) {
 		$flags = wp_parse_args( $flags, array(
 			'current'   => true,
 			'parent'    => true,
